10904d847cd6bca8548e9d5b6b5dbf556430a3e5
Baker Jackson <jacba@utschools.ca>
2019-07-07 16:27:03 -0400

    added csv files and made dynamic webpage

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from sklearn.externals import joblib
-import numpy as np
+import pandas as pd
+import random
 
 app = Flask(__name__)
 
@@ -8,18 +9,20 @@ model = joblib.load('dtr.pkl')
 
 @app.route("/")
 def index():
-    x = []
-    for i in range(864):
-        x.append(0)
-    x[862] = 1  # sets property type to MF
-    x[860] = 8350  # sets lot size
-    x[859] = 118  # age of house in years
-    x[858] = 4772  # square footage
-    x[857] = 4.0  # bathrooms
-    x[856] = 12  # bedrooms
-    prediction = model.predict([x])
+    data = pd.read_csv("Boston_March2018.csv")
+    proptypedummy = pd.get_dummies(data["PROPTYPE"])
+    data = pd.concat([data, proptypedummy], axis = 1)
+    citydummy = pd.get_dummies(data["CITY"])
+    data = pd.concat([data, citydummy], axis = 1)
+    zipdummy = pd.get_dummies(data["ZIP"])
+    data = pd.concat([data, zipdummy], axis = 1)
+    data = data.fillna(-999)
+    features = data[list(zipdummy.columns) + list(citydummy.columns) + ["BEDS", "BATHS", "SQFT", "AGE", "LOTSIZE", "CC", "MF", "SF"]]
+    x = random.randint(1, 5000)
+    address = data["ADDRESS"].iloc[x]
+    prediction = model.predict([features.iloc[x]])
     prediction = str(prediction).lstrip('[').rstrip(']').rstrip('.')
-    return render_template("index.html", prediction=prediction)
+    return render_template("index.html", prediction=prediction, address=address)
 
 if __name__=="__main__":
     app.run(debug=True)
